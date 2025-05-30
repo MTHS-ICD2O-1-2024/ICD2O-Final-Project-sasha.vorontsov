7cff0fe8b521011c2a1fb3a3be45afb9a53fdd76
Sasha-OK-bit <sasha.vorontsov@stu.ocsb.ca>
2025-05-30 16:59:15 +0000

    Added JS files and added all coding for the New Sonic game

---
+++ JS/game.js
@@ -0,0 +1,40 @@
+import SplashScene from "./splashScene.js"
+import TitleScene from "./titleScene.js"
+import MenuScene from "./menuScene.js"
+import GameScene from "./gameScene.js"
+
+const splashScene = new SplashScene()
+const titleScene = new TitleScene()
+const menuScene = new MenuScene()
+const gameScene = new GameScene()
+
+const config = {
+  type: Phaser.AUTO,
+  width: 1440,
+  height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false
+    }
+  },
+    
+    
+  backgroundColor: 0xffffff,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    //we place it in the middle of the page
+    autoCenter: Phaser.Scale.CENTER_BOTH
+    }
+  }
+
+  const game = new Phaser.Game (config)
+
+// load scenes
+// NOTE: remeber any "key" is global and CAN NOT be reused
+game.scene.add('splashScene', splashScene)
+game.scene.add('titleScene', titleScene) 
+game.scene.add('menuScene', menuScene) 
+game.scene.add('gameScene', gameScene) 
+//start title
+game.scene.start('splashScene')
---
+++ JS/levels.js
@@ -0,0 +1,28 @@
+// levels.js
+export const levels = [
+  {
+    name: "Windmill Islands",
+    enemies: [12],
+    goalReached: false,
+  },
+  {
+    name: "White Stones",
+    enemies: [20],
+    goalReached: false,
+  },
+  {
+    name: "Iced Wilds",
+    enemies: [15],
+    goalReached: false,
+  },
+  {
+    name: "Green and Greener Forest",
+    enemies: [24],
+    goalReached: false,
+  },
+  {
+    name: "Lava Bricks",
+    enemies: [21],
+    goalReached: false,
+  }
+]
